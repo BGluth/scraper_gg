3e1abcdd781ce6677bd2e5c76474dc15de3419a5
BGluth <gluthb@gmail.com>
2024-08-03 14:54:50 -0600

    Made `HydratableType` work nicely with `deref`
    - Want to make hydration "lazy" and work in the background without the
      user having to deal with it.
    - Ended up using an `UnsafeCell`... Not sure if I can avoid this, but if
      there's a way to not have to use unsafe, then I might switch to that
      in the future.

--- normalized_data/src/data_types.rs
+++ normalized_data/src/data_types.rs
@@ -1,6 +1,6 @@
-use std::fmt::Debug;
+use std::{cell::UnsafeCell, fmt::Debug, ops::Deref, rc::Rc};
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Serializer};
 
 pub struct NormalizedOrigin();
 impl DataOrigin for NormalizedOrigin {}
@@ -45,12 +45,82 @@ where
 
 pub trait DataOrigin {}
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub enum HydratableType<D, H>
+#[derive(Clone, Debug)]
+/// A wrapper type that is needed to be able to implicitly hydrate to the inner enum without the user being aware.
+pub struct HydratableType<D, H>(Rc<UnsafeCell<HydratableTypeInner<D, H>>>)
+where
+    D: Clone + Hydratable<Hydrated = H> + Hydratable,
+    H: Dehydrateable;
+
+/// Fun deref behavior. Essentially the goal is to tr
+impl<D, H> Deref for HydratableType<D, H>
+where
+    D: Clone + Hydratable<Hydrated = H> + Hydratable,
+    H: Dehydrateable,
+{
+    type Target = H;
+
+    fn deref(&self) -> &Self::Target {
+        let inner_ptr = self.0.get();
+
+        // I can't find another way to do this without using an `UnsafeCell`.
+        unsafe { (*inner_ptr).get_or_hydrate() }
+    }
+}
+
+impl<D, H> Serialize for HydratableType<D, H>
+where
+    D: Clone + Hydratable<Hydrated = H> + Hydratable,
+    H: Dehydrateable,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        todo!()
+    }
+}
+
+impl<'de, D, H> Deserialize<'de> for HydratableType<D, H>
+where
+    D: Clone + Hydratable<Hydrated = H> + Hydratable,
+    H: Dehydrateable,
+{
+    fn deserialize<De>(deserializer: De) -> Result<Self, De::Error>
+    where
+        De: serde::Deserializer<'de>,
+    {
+        todo!()
+    }
+}
+
+#[derive(Clone, Debug)]
+enum HydratableTypeInner<D, H>
 where
     D: Hydratable<Hydrated = H> + Hydratable,
     H: Dehydrateable,
 {
     Dehydrated(D),
     Hydrated(H),
+}
+
+impl<D, H> HydratableTypeInner<D, H>
+where
+    D: Clone + Hydratable<Hydrated = H> + Hydratable,
+    H: Dehydrateable,
+{
+    fn get_or_hydrate(&mut self) -> &H {
+        match self {
+            HydratableTypeInner::Dehydrated(d) => {
+                *self = Self::Hydrated(d.clone().hydrate());
+
+                // Haha this is bizarre... I can not figure out a better way to make this work though.
+                match self {
+                    HydratableTypeInner::Dehydrated(_) => unreachable!(),
+                    HydratableTypeInner::Hydrated(h) => h,
+                }
+            }
+            HydratableTypeInner::Hydrated(h) => h,
+        }
+    }
 }
